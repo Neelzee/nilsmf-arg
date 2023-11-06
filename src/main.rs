5f7858ca6605e7a1d87bc642e2b133254fd8da69
Neelzee <nilsien2001@gmail.com>
2023-11-06 23:31:02 +0100

    added project and image endpoint

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use actix_web::{get, post, App, HttpResponse, HttpServer, Responder, web, http};
 use std::fs::File;
 use std::io::{self, Read};
 use actix_cors::Cors;
+use walkdir::WalkDir;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -38,6 +39,54 @@ async fn get_file(path: web::Path<String>) -> impl Responder {
     HttpResponse::NotFound().insert_header(("Access-Control-Allow-Origin", "*")).body("File not found")
 }
 
+#[get("/image/{file}")]
+async fn get_image(path: web::Path<String>) -> impl Responder {
+    let file_path = String::from("./images/".to_string() + &path.into_inner());
+
+    if let Ok(res) = web::block(|| std::fs::read(file_path)).await {
+        if let Ok(con) = res {
+            return HttpResponse::Ok()
+                .insert_header(("Access-Control-Allow-Origin", "*"))
+                .content_type("image/png")
+                .body(con);
+        }
+    }
+    HttpResponse::NotFound().insert_header(("Access-Control-Allow-Origin", "*")).body("File not found")
+}
+
+#[get("/article/projects")]
+async fn get_projects() -> impl Responder {
+    let res: Result<Vec<String>, io::Error> = async {
+        let mut file_contents: Vec<String> = Vec::new();
+
+        for entry in WalkDir::new("./markdown/projects").into_iter().filter_map(|e| e.ok()) {
+            if entry.file_type().is_file() {
+                let file_path = entry.path();
+
+                let mut file = File::open(file_path)?;
+                let mut file_content = String::new();
+                file.read_to_string(&mut file_content)?;
+
+                file_contents.push(file_content);
+            }
+        }
+
+        Ok(file_contents)
+    }.await;
+    
+    if let Ok(con) = res {
+
+        let json_data = web::Json(con);
+
+        return HttpResponse::Ok()
+            .insert_header(("Access-Control-Allow-Origin", "*"))
+            .insert_header(("Content-Type", "text/markdown"))
+            .json(json_data);
+    }
+
+    HttpResponse::NotFound().insert_header(("Access-Control-Allow-Origin", "*")).body("File not found")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
@@ -48,7 +97,9 @@ async fn main() -> std::io::Result<()> {
             // fn service(f: impl Fn() -> impl Responder)
             .service(hello)
             .service(echo)
+            .service(get_projects)
             .service(get_file)
+            .service(get_image)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
