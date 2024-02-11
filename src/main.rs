a7dafdbe678864e42b4edd7e9a094172132b00d1
Neelzee <nilsien2001@gmail.com>
2024-02-11 22:27:44 +0100

    reworked

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
-mod utils;
 mod api;
+mod db;
+mod utils;
 
+use actix_cors::Cors;
 use actix_web::{App, HttpServer};
 use api::articles::get_article;
 use api::images::get_image;
 use api::projects::{get_project, get_projects};
-use actix_cors::Cors;
-
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        let cors = Cors::permissive();
-
         App::new()
-            .wrap(cors)
+            .wrap(Cors::permissive())
             // fn service(f: impl Fn() -> impl Responder)
             .service(get_projects)
             .service(get_article)
---
+++ src/utils/consts.rs
@@ -0,0 +1,1 @@
+pub const ROOT: &str = "db";
