8bbc932cbb7624f4f44df47776e2d84ad83e3681
Neelzee <nilsien2001@gmail.com>
2024-02-13 08:09:53 +0000

    https

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,31 @@ mod api;
 // mod db;
 mod utils;
 
+use std::{
+    fs::File,
+    io::{self, Read as _},
+};
 use actix_cors::Cors;
 use actix_web::{App, HttpServer};
 use api::articles::get_article;
 use api::images::get_image;
 use api::projects::{get_project, get_projects};
+use openssl::{
+    pkey::{PKey, Private},
+    ssl::{SslAcceptor, SslMethod},
+};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+
+    // set the encrypted private key
+    builder
+        .set_private_key(&load_encrypted_private_key())
+        .unwrap();
+
+        builder.set_certificate_chain_file("/etc/letsencrypt/live/nilsmf.com/cert.pem").unwrap();
+
     HttpServer::new(|| {
         App::new()
             .wrap(Cors::permissive())
@@ -19,8 +36,16 @@ async fn main() -> std::io::Result<()> {
             .service(get_project)
             .service(get_image)
     })
-    .bind(("0.0.0.0", 8080))?
+    .bind_openssl(("0.0.0.0", 8080), builder)?
     // .bind(("127.0.0.1", 8080))?
     .run()
     .await
+}
+
+fn load_encrypted_private_key() -> PKey<Private> {
+    let mut file = File::open("/etc/letsencrypt/live/nilsmf.com/privkey.pem").unwrap();
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).expect("Failed to read file");
+
+    PKey::private_key_from_pem_passphrase(&buffer, b"password").unwrap()
 }
